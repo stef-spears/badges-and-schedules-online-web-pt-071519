bcc3911de26cd31e420094c183c911518d693b38
Stef Spears <spears.sj87@gmail.com>
2019-07-22 19:11:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers.each do |speaker|
+     "Hello, my name is #{speaker}."
+end
+end
+
+def assign_rooms(speakers)
+end
+
+def printer()
+end
